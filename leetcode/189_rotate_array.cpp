b1bc8b9fd9bb364becdc97e9c090c739c2e695a0
adam <admr0805@gmail.com>
2026-08-23 23:00:05 -0400

    Replace Rotate Array extra vector with in-place reversals.

--- leetcode/189_rotate_array.cpp
+++ leetcode/189_rotate_array.cpp
@@ -4,19 +4,27 @@ using namespace std;
 
 class Solution {
 public:
+	void reverse(int* begin, int* end) {
+		int* lp = begin; 
+		int* rp = end-1; 
+		int temp; 
+
+		while (lp < rp) {
+			temp = *lp; 
+			*lp = *rp; 
+			*rp = temp; 
+
+			++lp; 
+			--rp; 
+		}	
+	}
+
 	void rotate(vector<int>& nums, int k) {
-		vector<int> v; 
 		int s = k % nums.size(); 
 
-		for(size_t i = nums.size()-s; i < nums.size(); ++i) {
-			v.push_back(nums[i]); 
-		}
+		reverse(nums.data(), nums.data()+nums.size()); 
+		reverse(nums.data(), nums.data()+s);
+		reverse(nums.data()+s, nums.data()+nums.size()); 
 
-		for(size_t i = 0; i < nums.size()-s; ++i) {
-			v.push_back(nums[i]);	
-		}
-
-		nums = v; 
-		
 	}
 };
