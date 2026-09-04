dde90af60f76356162e44e0844a704290ccb7772
adam <admr0805@gmail.com>
2026-09-04 14:29:24 -0400

    feat(leetcode): solve top k frequent elements
    use frequency buckets to return the most common values in linear time

--- leetcode/347_top_k_frequent_elements.cpp
+++ leetcode/347_top_k_frequent_elements.cpp
@@ -1,10 +1,30 @@
 #include <vector>
+#include <unordered_map> 
 
 using namespace std;
 
 class Solution {
 public:
 	vector<int> topKFrequent(vector<int>& nums, int k) {
+		unordered_map<int, int> freq; 
+		for(int x : nums) freq[x]++;
+
+		vector<vector<int>> buckets(nums.size()+1); // 1 bucket per frequency 
+		for(const auto& [num, count] : freq) {
+			buckets[count].push_back(num); 
+		}
 		
+		vector<int> ans; 
+		for(int i = nums.size(); i > 0; i--) {
+			for(int j = 0; j < buckets[i].size(); j++) {
+				ans.push_back(buckets[i][j]);
+
+				if (ans.size() == k) {
+					return ans; 
+				}
+			}
+		}
+
+		return ans; 
 	}
 };
